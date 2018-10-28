934bae6a4588cab7ba2b34af1125925ba11db292
insu.choi <ocean2144@gmail.com>
2018-10-28 18:22:19 +0900

    Project delete

--- MallServer/src/main/java/com/csai/mall/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.csai.mall.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.csai.mall.model.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-  // List<User> findByName(String name);
-}
